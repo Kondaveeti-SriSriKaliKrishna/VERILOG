2a348c1d1401cb490c494cbc22949aeb0651eedc
KONDAVEETI SRI SRI KALI KRISHNA <ksrisrikalikrishna@gmail.com>
2026-08-25 21:20:00 +0530

    Update testbench to use behavioral comparator

--- Combinational Modules/11. Multibit Comparator/tb_multibit_comparator.v
+++ Combinational Modules/11. Multibit Comparator/tb_multibit_comparator.v
@@ -1,19 +1,20 @@
 //`include "multibit_comparator_dataflow.v"
-`include "multibit_comparator_struc.v"
-//`include "comparator_2bit_behv.v"
-//`include "comparator_2bit_struc.v"
+//`include "nbit_comparator_behavioral.v"
+`include "multibit_comparator_behavioral.v"
 module tb;
-reg [3:0] a,b;
-wire aGb,aLb,aEb;
-integer i;
-comparator_4bit_struc dut(a,b,aGb,aLb,aEb);
-initial begin
- for(i=0;i<256;i=i+1) begin
- {a,b}=i;
- #2;
- $display("a=%b,b=%b,aGb=%b,aLb=%b,aEb=%b",a,b,aGb,aLb,aEb);
- end
- end
+	reg [3:0] a,b;
+	wire aGb,aLb,aEb;
+	integer i;
+
+multibit_comparator dut(a,b,aGb,aLb,aEb);
+
+	initial begin
+ 		for(i=0;i<256;i=i+1) begin
+ 			{a,b}=i;
+ 			#2;
+			 $display("a=%b,b=%b,aGb=%b,aLb=%b,aEb=%b",a,b,aGb,aLb,aEb);
+ 		end
+ 	end
  endmodule
 
 
