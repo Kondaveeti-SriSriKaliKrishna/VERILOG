4ac107e89bd735cafcb0319f75b7e04ae9430729
KONDAVEETI SRI SRI KALI KRISHNA <ksrisrikalikrishna@gmail.com>
2026-08-28 19:41:22 +0530

    Add files via upload

---
+++ Sequential Modules/2. Flip Flop's/JK Flip Flop/jk_ff.v
@@ -0,0 +1,19 @@
+module jk_ff (
+    input clk, rst,
+    input j, k,
+    output reg q
+);
+
+	always @(posedge clk) begin
+		if(rst)
+			q <= 1'b0;
+		else begin
+    		case ({j, k})
+        		2'b00: q <= q;       // Hold
+        		2'b01: q <= 1'b0;   // Reset
+        		2'b10: q <= 1'b1;   // Set
+        		2'b11: q <= ~q;     // Toggle
+    		endcase
+		end
+	end
+endmodule
